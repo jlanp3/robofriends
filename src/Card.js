66523fc031c02bf2423f3e92ba35d336da16376a
jlanp3 <jlanp3@gmail.com>
2023-02-25 22:19:39 -0300

    added tachyons

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const Card = () => {
     return (
-        <div>
+        <div className="bg-white center mw5 ba b--black-10 mv4">
+            <div class="pv2 ph3">
+                <h1 class="f6 ttu tracked">Daily News Co.</h1>
+            </div>
             <img src="https://robohash.org/test?200x200" alt="Robot" />
-            <div>
+            <div class="pa3">
                 <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <p className="gray db pv2">jane.doe@gmail.com</p>
             </div>
         </div>
     )
